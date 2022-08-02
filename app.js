1a727c7f2e1283927031bb6a3c0a854cbca5b0df
vellfr00 <vellfr00@gmail.com>
2022-08-02 20:19:13 +0200

    Server and database initialization

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require('dotenv').config();
 
-var app = express();
+const indexRouter = require('./routes/index');
 
-app.use(logger('dev'));
+const app = express();
+
+app.use(cors);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-module.exports = app;
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+//SERVER START
+db.once('open', function(error) {
+    if(error) {
+        console.log("Database connection error: " + error);
+    } else {
+        console.log("Database connected, starting server...");
+
+        app.listen(process.env.PORT, function(err) {
+            if(err) {
+                console.log("Cannot start server: " + err);
+            } else {
+                console.log("Server started successfully on port " + process.env.PORT);
+            }
+        });
+    }
+});
