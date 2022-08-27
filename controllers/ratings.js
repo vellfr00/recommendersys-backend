9a9c20c4acf99ad6eb7ae00ef1d57b9dbd6354f5
vellfr00 <vellfr00@gmail.com>
2022-08-27 17:40:19 +0200

    Get user, ratings

--- controllers/ratings.js
+++ controllers/ratings.js
@@ -3,6 +3,71 @@ const Movies = require('../models/movie');
 const Preferences = require('../models/preferences');
 
 const ratingsController = {
+    //GET Handler - Get users rated movies or to rate movies
+    getRatings: (req, res, next) => {
+        let username = req.params.username;
+
+        switch(req.query.rated) {
+            case 'true':
+                Ratings.find({username: username}).populate('movie')
+                    .then((document) => {
+                        if(!document) {
+                            res.status(404);
+                            next("No ratings for user " + username);
+                            return;
+                        }
+
+                        res.status(200);
+                        res.json(document);
+                        res.end();
+                    }).catch((error) => {
+                    console.log("Cannot get user ratings: " + error);
+
+                    res.status(500);
+                    res.json({message: "Cannot get user ratings: " + error.message});
+                    res.end();
+                });
+
+                break;
+            case 'false':
+                Preferences.findOne({username: username}, {_id: 0, toRate: 1})
+                    .then((document) => {
+                        if (!document) {
+                            res.status(404);
+                            next("Cannot get to rate movies: cannot find user " + username);
+                            return;
+                        }
+
+                        if(document.toRate.length === 0) {
+                            res.status(404);
+                            next("No movies to rate for user " + username);
+                            return;
+                        } else {
+                            let toRateIds = document.toRate.map((movie) => movie.movieId);
+
+                            return Movies.find({movieId: {$in: toRateIds}})
+                                .then((movies) => {
+                                    res.status(200);
+                                    res.json(movies);
+                                    res.end();
+                                })
+                        }
+                    }).catch((error) => {
+                    console.log("Cannot get to rate movies: " + error);
+
+                    res.status(500);
+                    res.json({message: "Cannot get to rate movies: " + error.message});
+                    res.end();
+                });
+
+                break;
+            default:
+                res.status(400);
+                next("Invalid rated status type");
+                return;
+        }
+    },
+
     //POST Handler - Add new rating to movie by its movieId and update probIndex
     addRating: async (req, res, next) => {
         let max, id = req.params.movieId;
--- routes/ratings.js
+++ routes/ratings.js
@@ -3,6 +3,7 @@ const ratingsRouter = express.Router();
 
 const ratingsController = require('../controllers/ratings');
 
+ratingsRouter.get('/:username', ratingsController.getRatings);
 ratingsRouter.post('/:movieId', ratingsController.addRating);
 
 module.exports = ratingsRouter;
